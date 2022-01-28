517bd4b565d04235fa1aed977d3e1edf80e98083
Андрей Марфин <andreymarfinpost@gmail.com>
2022-01-28 16:39:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean isGreenLight = false;
+
+        int speedOfPlayer1 = 1;
+        int speedOfPlayer2 = 1;
+        int speedOfPlayer3 = 0;
+
+        int outsiders = 0;
+        if (isGreenLight) {
+            System.out.println("Количество выбывающих игроков: " + outsiders);
+        } else {
+
+            if (speedOfPlayer1 > 0) {
+                outsiders = outsiders + 1;
+            }
+            if (speedOfPlayer2 > 0) {
+                outsiders = outsiders + 1;
+            }
+            if (speedOfPlayer3 > 0) {
+                outsiders = outsiders + 1;
+            }
+            System.out.println("Количество выбывающих игроков: " + outsiders);
+        }
+    }
+}
